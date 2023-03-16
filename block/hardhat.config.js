8a76a28290819438cdccf9e08ba5f68470d95413
getzelus <chamoloss@live.fr>
2023-03-16 14:45:46 +0100

    verify on polygon mumbai

--- block/hardhat.config.js
+++ block/hardhat.config.js
@@ -4,12 +4,28 @@ require("dotenv").config({ path: ".env" });
 const QUICKNODE_HTTP_URL = process.env.QUICKNODE_HTTP_URL;
 const PRIVATE_KEY = process.env.PRIVATE_KEY;
 
+const POLYGONSCAN_KEY = process.env.POLYGONSCAN_KEY;
+const MUMBAI_URL = process.env.MUMBAI_URL;
+
 module.exports = {
+
   solidity: "0.8.7",
+
   networks: {
     sepolia: {
       url: QUICKNODE_HTTP_URL,
       accounts: [PRIVATE_KEY],
     },
+    mumbai: {
+      url: MUMBAI_URL,
+      accounts: [PRIVATE_KEY],
+    },
   },
+
+  etherscan: {
+    apiKey: {
+      polygonMumbai: POLYGONSCAN_KEY,
+    }
+  }
+
 };
